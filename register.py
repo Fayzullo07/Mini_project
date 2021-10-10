79582637211db87d81d6154d2d09cb8904c389d9
F4995875f <jorayevfayzillo77@gmail.com>
2021-10-10 12:59:09 +0500

    Reja tuzildi

---
+++ register.py
@@ -0,0 +1,29 @@
+class Users:
+    def __init__(self):
+        self.name = None
+        self.age = None
+        self.login = None
+        self.password = None
+
+    def entrance_system(self):
+        pass
+
+    def register(self):
+        pass
+
+    def log_in(self):
+        pass
+
+    def update_login_and_password(self):
+        pass
+
+    def delete_account(self):
+        pass
+
+    def log_out(self):
+        pass
+
+
+users = Users()
+users.entrance_system()
+
