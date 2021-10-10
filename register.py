06f9842c9597cdc77c9995507c4692798a141496
F4995875f <jorayevfayzillo77@gmail.com>
2021-10-10 13:09:51 +0500

    1 qadam

--- register.py
+++ register.py
@@ -1,12 +1,28 @@
+import os
+
+
 class Users:
     def __init__(self):
         self.name = None
         self.age = None
         self.login = None
         self.password = None
+        self.options = ['1', '2']
 
     def entrance_system(self):
-        pass
+        self.clear_screen()
+        self.print_first_message()
+        input_choice = input(">>>: ").strip()
+        while self.is_string_empty(input_choice) or input_choice not in self.options:
+            self.clear_screen()
+            self.print_first_message()
+            input_choice = input(">>>: ").strip()
+
+        if input_choice == self.options[0]:
+            self.register()
+        else:
+            self.log_in()
+
 
     def register(self):
         pass
@@ -22,6 +38,26 @@ class Users:
 
     def log_out(self):
         pass
+
+    # __________Messages_____________
+
+    @staticmethod
+    def print_first_message():
+        print("""
+        Register [1]
+        Login    [2]
+        """)
+
+    # __________Functions______________
+
+    @staticmethod
+    def clear_screen():
+        os.system("clear")
+
+    @staticmethod
+    def is_string_empty(str_):
+        return not bool(str_)
+
 
 
 users = Users()
